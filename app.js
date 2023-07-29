cf8449df807f3bffa693776932f7323a3e08bed7
Vika <liusikova.vika@yandex.ru>
2023-07-29 18:54:52 +0300

    временное решение авторизации

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { routes } = require('./routes');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+// подключаемся к серверу mongo
+mongoose.connect('mongodb://localhost:27017/mestodb')
+  .then(() => {
+    console.log('Database connected.');
+  })
+  .catch((err) => {
+    console.log('Error on database connection');
+    console.error(err);
+  });
+
+// временное решение авторизации пользователя
+app.use((req, res, next) => {
+  req.user = {
+    _id: '1234567890',
+  };
+
+  next();
+});
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`is running on port ${PORT}`);
+});
---
+++ controllers/createUser.js
@@ -0,0 +1,10 @@
+const { User } = require('../models/user');
+
+async function createUser(req, res) {
+  const { name, about, avatar } = req.body;
+
+  const user = await User.create({ name, about, avatar });
+  res.send(user);
+}
+
+module.exports = { createUser };
