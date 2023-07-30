e8156ea7fc5232149035c421233f71cc345a8624
Vika <liusikova.vika@yandex.ru>
2023-07-30 11:04:44 +0300

    test

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express');
-
 const mongoose = require('mongoose');
+const { routes } = require('./routes');
 
 const { PORT = 3000 } = process.env;
 
 // подключаемся к серверу mongo
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  // useCreateIndex: true,
-  // useFindAndModify: false,
-});
+mongoose.connect('mongodb://localhost:27017/mestodb')
+  .then(() => {
+    console.log('Database connected.');
+  })
+  .catch((err) => {
+    console.log('Error on database connection');
+    console.error(err);
+  });
 
 const app = express();
 
