28fc3dfba6740d9facf4955fc03ecc0f8820d95a
Vika <liusikova.vika@yandex.ru>
2023-08-08 22:19:19 +0300

    main Создайте роут для логина и регистрации

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,18 @@ const {
   cards,
 } = require('./cards')
 
+const {
+ login, createUser
+} = require('../controllers/users')
+
 const routes = express.Router()
+
+routes.post('/signup', express.json(), createUser)
+routes.post('/signin', express.json(), login)
 
 routes.use('/users', users)
 routes.use('/cards', cards)
+
 routes.all('*', (req, res) => {
   const err = new Error('Неверный адрес запроса')
   err.name = 'NotFoundError'
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const {
-  getAllUsers, getUser, createUser, updateUser, updateAvatar,
+  getAllUsers, getUser, updateUser, updateAvatar,
 } = require('../controllers/users')
 
 const users = express.Router()
 
 users.get('/', getAllUsers)
 users.get('/:userId', getUser)
-users.post('/', express.json(), createUser)
+// users.post('/', express.json(), createUser)
 users.patch('/me', express.json(), updateUser)
 users.patch('/me/avatar', express.json(), updateAvatar)
 
